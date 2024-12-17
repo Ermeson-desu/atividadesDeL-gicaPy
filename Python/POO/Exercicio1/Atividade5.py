7eb78b8b29e86e68f358bf255f2db8fba5cd24f7
Ermeson-desu <ermesondesu@gmail.com>
2024-12-17 07:53:34 -0300

    update

--- Python/POO/Exercicio1/Atividade5.py
+++ Python/POO/Exercicio1/Atividade5.py
@@ -3,7 +3,8 @@ notas = [7.5, 8.0, 6.5, 9.0, 7.0]
 def calcular_media(lista):
     somatorio = 0
     for i in lista:
-        somatorio = somatorio + i    
-    print(f"A média da lista é {somatorio/len(lista)}")
+        somatorio = somatorio + i
+    res = somatorio/len(lista)    
+    print(f"A média da lista é {res}")
 
 calcular_media(notas)
