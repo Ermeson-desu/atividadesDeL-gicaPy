b671d931b180a2f9135b9e43d3edf26468dfb2b0
Ermeson-desu <ermesondesu@gmail.com>
2024-12-17 08:19:10 -0300

    update

--- Python/POO/Exercicio1/Atividade3.py
+++ Python/POO/Exercicio1/Atividade3.py
@@ -1,7 +1,7 @@
 entrada_usuario = int(input("Digite um numero inteiro: "))
 
 def tabuada(numero_usuario):
-    for i in range(11):
+    for i in range(1,11):
         resultado = numero_usuario * i
         print(f"{numero_usuario} * {i} = {resultado}")
 
--- Python/POO/Exercicio1/Atividade4.py
+++ Python/POO/Exercicio1/Atividade4.py
@@ -1,8 +1,10 @@
-numeros = [2, 7, 10, 15, 20, 33, 42]
 
 def numeros_pares_lista(lista):
     for i in range(len(lista)):
         if lista[i] % 2 == 0:
             print(lista[i])
+
+
+numeros = [2, 7, 10, 15, 20, 33, 42]
 
 numeros_pares_lista(numeros)
